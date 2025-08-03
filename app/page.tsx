424d5b755e5a2e0761bdf68989b5ee712ab8b652
YehyunLee <glad7cu@gmail.com>
2025-08-03 00:31:24 -0400

    V2

--- app/page.tsx
+++ app/page.tsx
@@ -1354,7 +1354,7 @@ Here's the person I see: This is ${person.name}, who is your ${person.relationsh
                                   d="M15 10l4.553-2.276A1 1 0 0121 8.618v6.764a1 1 0 01-1.447.894L15 14M5 18h8a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2z"
                                 />
                               </svg>
-                              <span>🚀 Start Full Screen Camera</span>
+                              <span>Start Full Screen Camera</span>
                             </>
                           ) : (
                             <>
